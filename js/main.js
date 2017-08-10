eb2f07778a732ee04d1634ab329eb3a7a905a39b
marzoonobrega <marzoo.nobrega@gmail.com>
2017-08-10 12:01:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function randomPassword(length)
+{
+  chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  pass = "";
+  for(x=0;x<length;x++)
+  {
+    i = Math.floor(Math.random() * 62);
+    pass += chars.charAt(i);
+  }
+  return pass;
+}
+function formSubmit()
+{
+  passform.passbox.value = randomPassword(passform.length.value);
+  return false;
+}
